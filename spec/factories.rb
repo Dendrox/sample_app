2e5ed5b1050f783bdaf59a2722ca9716fbcfe6d4
Rody Kirwan <rody_kirwan@yahoo.co.uk>
2013-04-19 20:18:12 +0100

    testing gravatar

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do 
+    factory :user do
+    	name "Rody Kirwan"
+    	email "rody_kirwan@yahoo.co.uk"
+    	password "Dendrox1"
+    	password_confirmation "Dendrox1"
+
+    end
+	
+end
